0b68a932b6e5c4a1441c3781b9d7a68258bbe12a
Matalin Racster <matalinracster@149-161-162-124.dhcp-bl.indiana.edu>
2019-10-09 15:46:13 -0400

    idk

--- main1.py
+++ main1.py
@@ -14,16 +14,16 @@ SCREEN_HEIGHT = 600
 MARGIN = 30
 SCREEN_TITLE = "Particle Exercise"
 
-PARTICLE_MIN_SCALE = 0.01
+PARTICLE_MIN_SCALE = 0.1
 PARTICLE_MAX_SCALE = 0.08
-PARTICLE_MIN_X = -20
-PARTICLE_MAX_X = 20
+PARTICLE_MIN_X = -40
+PARTICLE_MAX_X = 40
 PARTICLE_VELOCITY_X = 0
 PARTICLE_VELOCITY_Y = 4
-PARTICLE_MIN_AX = -0.1
-PARTICLE_MAX_AX = 0.1
-PARTICLE_MIN_AY = -0.1
-PARTICLE_MAX_AY = 0.1
+PARTICLE_MIN_AX = -0.03
+PARTICLE_MAX_AX = 0.03
+PARTICLE_MIN_AY = -0.03
+PARTICLE_MAX_AY = 0.03
 PARTICLE_MIN_DECAY = 0.001
 PARTICLE_MAX_DECAY = 0.01
 
@@ -42,15 +42,15 @@ class Particle(arcade.Sprite):
 
         self.particle_colors = [
             (open_color.red_5, 4)
-            ,(open_color.red_4, 5)
+            ,(open_color.orange_4, 5)
             ,(open_color.red_3, 6)
             ,(open_color.red_2, 7)
             ,(open_color.red_1, 8)
-            ,(open_color.teal_1, 8)
-            ,(open_color.teal_2, 7)
-            ,(open_color.teal_3, 6)
-            ,(open_color.teal_4, 5)
-            ,(open_color.teal_5, 4)
+            ,(open_color.orange_1, 8)
+            ,(open_color.orange_2, 7)
+            ,(open_color.orange_3, 6)
+            ,(open_color.orange_4, 5)
+            ,(open_color.orange_5, 4)
         ]
         (self.color, self.lifetime) = self.particle_colors[self.color_pos]
         self.alive = True
@@ -110,7 +110,7 @@ class Window(arcade.Window):
             decay = random.uniform(PARTICLE_MIN_DECAY,PARTICLE_MAX_DECAY)
             scale = random.uniform(PARTICLE_MIN_SCALE,PARTICLE_MAX_SCALE)
             #Particle(asset, sprite scale, initial position [x], initial position [y], velocity [x], velocity [y], acceleration [x], acceleration [y], scale decay)
-            particle = Particle('circle_05',scale,x,y,dx,dy,ax,ay,decay)
+            particle = Particle('flame_02',scale,x,y,dx,dy,ax,ay,decay)
 
             self.particle_list.append(particle)
 
